fe264a304b5794110b97572eb78b01492e83ce80
Codnpix <pierrejarriges@gmail.com>
2018-11-20 09:56:03 +0100

    creation classes map JS, pas encore interactive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,63 @@
 /*javascript*/
+let myBody = document.querySelector("body");
+myBody.style.backgroundImage = "url('img/lluvia-animada.gif')";
+myBody.style.backgroundSize = "cover";
+let canvasParent = document.getElementById('canvasParent');
+let ctx;
+
+
+
+class MapArea {
+    
+    constructor(width, height, parent){
+        this.canvas = document.createElement("canvas");
+        parent.appendChild(this.canvas);
+        this.context = this.canvas.getContext("2d");
+        this.width = width;
+        this.height = height;
+    }
+    
+    init() {
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.interval = setInterval(updateMap, 50);
+    }
+    clear() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+
+class Map {
+    
+    constructor(x, y, width, height, source) {
+        
+        this.x = x;
+        this.y = y;
+        this.moveX = 0;
+        this.moveY = 0;
+        this.mapImage = new Image();
+        this.mapImage.src = source;
+        this.width = this.mapImage.width;
+        this.height = this.mapImage.height; 
+    }
+    
+    update() {
+        ctx = myMapArea.context;
+        ctx.drawImage(this.mapImage, this.x, this.y, this.width, this.height);
+    }
+}
+
+function updateMap() {
+    myMap.update();
+}
+
+window.onload = function() {
+    myMapArea.init();
+}
+
+let myMapArea = new MapArea(500, 500, canvasParent);
+let myMap = new Map(-50, -50, 1911, 1781, "img/carte-detaillee-france.jpg");
+
+myMap.update();
+
