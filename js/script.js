94f3a672b9acc114872fd7cf0c46b31283f1371e
Codnpix <pierrejarriges@gmail.com>
2018-11-22 10:47:43 +0100

    affichage resultats sur carte

--- js/script.js
+++ js/script.js
@@ -23,3 +23,60 @@ for (let part of dayParts) {
         part.setAttribute("class", "dayPart active");
     }
 }
+
+/*affichage type donnée sur carte*/
+//onglets du menu de recherche (seulement 3 pour le moment)
+let prevSearchTab = document.getElementById("prevSearch");
+let tempSearchTab = document.getElementById("tempSearch");
+let windSearchTab = document.getElementById("windSearch");
+
+let prevDataArray = document.getElementsByClassName("prevGrid");
+let tempDataArray = document.getElementsByClassName("tempGrid");
+let windDataArray = document.getElementsByClassName("windGrid");
+
+prevSearchTab.onclick = function() {
+    for (prevData of prevDataArray) {
+        prevData.style.display = "block";
+        for (tempData of tempDataArray) {
+            tempData.style.display = "none";
+        }
+        for (windData of windDataArray) {
+            windData.style.display = "none";
+        }
+    }
+}
+
+tempSearchTab.onclick = function() {
+    for (tempData of tempDataArray) {
+        tempData.style.display = "block";
+        for (prevData of prevDataArray) {
+            prevData.style.display = "none";
+        }
+        for (windData of windDataArray) {
+            windData.style.display = "none";
+        }
+    }
+}
+/*couleur polices temérature en fonction de la valeur*/
+let tmpVals = document.getElementsByClassName("tmpVal");
+for (tmpVal of tmpVals) {
+    let nodeTmpVal = parseInt(tmpVal.textContent);
+    if (nodeTmpVal >= 15 && nodeTmpVal < 25) {
+        tmpVal.style.color = "#092";
+    }
+    else if (nodeTmpVal >= 25) {
+        tmpVal.style.color = "#f55";
+    }
+}
+
+windSearchTab.onclick = function() {
+    for (windData of windDataArray) {
+        windData.style.display = "flex";
+        for (tempData of tempDataArray) {
+            tempData.style.display = "none";
+        }
+        for (prevData of prevDataArray) {
+            prevData.style.display = "none";
+        }
+    }
+}
